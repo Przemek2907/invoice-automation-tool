ecf188e96c4462695c7171134b31030df67a7ea9
Przemysław Żochowski <przemyslaw.zochowski@stepwise.pl>
2021-08-17 18:26:03 +0200

    Switching from tuple to dict to save the file results

--- service/user_menu.py
+++ service/user_menu.py
@@ -10,15 +10,23 @@ def compose_user_menu():
     search_invoice_directory = obtain_user_data("Podaj adres katalogu, w którym szukać faktur:\n")
     save_data = obtain_user_data("Czy chcesz zapisać dane do wykorzystania w kolejnych miesiącach (Y/N)?:\n")
 
-    user_data = (accountant_name, accountant_surname, accountant_mail, search_mail_account, search_invoice_directory)
+    user_data = (
+        ('accountant_name', accountant_name),
+        ('accountant_surname', accountant_surname),
+        ('accountant_mail', accountant_mail),
+        ('search_mail_account', search_mail_account),
+        ('search_invoice_directory', search_invoice_directory)
+    )
+
+    user_data_dict = dict(user_data)
 
     if save_data.lower() == 'y':
-        save_data_to_file(user_data, input("Podaj ścieżkę zapisu pliku:\n"))
+        save_data_to_file(user_data_dict, input("Podaj ścieżkę zapisu pliku:\n"))
     else:
-        return user_data
+        return user_data_dict
 
 
 def save_data_to_file(user_data_to_file, file_path_to_save_data):
     with open(file_path_to_save_data, 'w') as save_data_file:
-        for answer in user_data_to_file:
-            save_data_file.write(answer + "\n")
+        for key, value in user_data_to_file.items():
+            save_data_file.write(key + ':' + value + "\n")
